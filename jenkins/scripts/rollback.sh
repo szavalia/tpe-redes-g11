14bdf744a574a8f8cd1ee3554a853419e1d255ed
Salus Zavalía <szavalia@itba.edu.ar>
2023-06-08 00:02:33 -0300

    remove extra script

--- jenkins/scripts/rollback.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get last commit on main branch
-LAST_COMMIT=$(git log -1 --format="%H" origin/main)
-echo $LAST_COMMIT
-
-# Revert the last commit
-git revert $LAST_COMMIT --no-edit
